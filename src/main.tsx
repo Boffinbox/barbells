09926e455e348820b1975af13c2ccb346abee02c
Boffinbox <44021545+Boffinbox@users.noreply.github.com>
2026-02-20 21:05:19 +0000

    add tanstack router and basic root route

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Barbells } from './barbells'
+import ReactDOM from 'react-dom/client'
 import "@fontsource/inter"
+import { RouterProvider, createRouter } from '@tanstack/react-router'
 
-createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <Barbells />
-    </StrictMode>,
-)
+// Import the generated route tree
+import { routeTree } from './routeTree.gen'
+
+// Create a new router instance
+const router = createRouter({
+    routeTree
+})
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+    interface Register
+    {
+        router: typeof router
+    }
+}
+
+// Render the app
+const rootElement = document.getElementById('root')!
+if (!rootElement.innerHTML)
+{
+    const root = ReactDOM.createRoot(rootElement)
+    root.render(
+        <StrictMode>
+            <RouterProvider router={router} />
+        </StrictMode>,
+    )
+}
