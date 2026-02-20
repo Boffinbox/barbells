5cda977c7cd00cb4b38d08ec3ba9616ce983a867
Boffinbox <44021545+Boffinbox@users.noreply.github.com>
2026-02-20 20:04:44 +0000

    add vite proxy setup and localhost ports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, type ServerOptions } from 'vite'
 import react from '@vitejs/plugin-react'
+
+let server: ServerOptions =
+{
+    port: 3000,
+    proxy: {
+        "/api": {
+            target: `https://localhost:5000`,
+            changeOrigin: true,
+            secure: false,
+        }
+    },
+}
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server
 })
